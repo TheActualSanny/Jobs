28abcd852bef4a224178f9f9a9196fbb4957e15b
Sanny <dugna1234@gmail.com>
2024-10-13 02:50:59 +0400

    Changed the name of the class, used a list comprehension and updated exception handling

--- jobsge/jobsge._finder.py
+++ jobsge/jobsge._finder.py
@@ -1,28 +1,29 @@
 import string
 import logging
 import json
-from services.logging_config import logger
+from utils.logging_config import logger
 
-class Find_Jobs():
+class FindJobs():
     def load_jobs(self):
         '''Loads the jobs.json file.'''
         try:
             with open('jobs.json', 'r') as file:
-                loaded_lobs = json.load(file)
-                return loaded_lobs
-        except (FileNotFoundError, json.JSONDecodeError):
-            logger.exception('Exception caught, please check the data again.')
+                loaded_jobs = json.load(file)
+                return loaded_jobs
+        except FileNotFoundError:
+            logger.exception('The file was not found, please check if the name is written correctly.')
             return None
+        except json.JSONDecodeError:
+            logger.exception('Could not load the JSON data.')
+            return None            
         
     def search_jobs(self, user_input):
         lowercase_input = user_input.lower()
         initial_jobs = self.load_jobs()
-        found_jobs = [] # The jobs that were found based on the user input will be stored here. It will be passed to a module in the db folder
 
         if initial_jobs:
-            for job in initial_jobs:
-                lower_position = job['position'].lower()
-                if lowercase_input in lower_position:
-                    found_jobs.append(job)
+            found_jobs = [job for job in initial_jobs if lowercase_input in job['position'].lower()]# The jobs that were 
+            # found based on the user input will be stored here. It will be passed to a module in the db folder
+            
         return found_jobs
                 
